da94b9ec6524b249457a0274d56a77d3bea14d02
Ruchi Mittal <ruchimittal2205@gmail.com>
2025-02-06 23:26:18 +0530

    Add files via upload

---
+++ EmployeeInfo.java
@@ -0,0 +1,13 @@
+public class EmployeeInfo
+{
+    public static void main(String[]args)
+    {
+        System.out.println("NAME  MAIL-ID  EMPLOYEE-CODE  PHONE");
+        System.out.println("Ravi  ravi@cse.com   p03161  9876543210");
+        System.out.println("Amit  amit@cse.com   p03162  8765432109");
+        System.out.println("Sonia sonia@cse.com  p03163  7895643290");
+        System.out.println("Jatin jatin@cse.com  p03164  9045904432");
+        System.out.println("Naina naina@cse.com  p03165  6785309890");
+    }
+
+}
