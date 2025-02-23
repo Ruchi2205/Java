afc1a32e21e4965cb32321376d465d0d6010ae63
Ruchi Mittal <ruchimittal2205@gmail.com>
2025-02-23 21:30:49 +0530

    Add files via upload

---
+++ Assignment 4/PostDecrementExample.java
@@ -0,0 +1,8 @@
+class PostDecrementExample{
+    public static void main(String[] args) {
+        int num=50;
+        int result=num--;
+        System.out.println("Post-Decrement: "+result);
+        System.out.println("After post-decrement: "+num);
+    }
+}
